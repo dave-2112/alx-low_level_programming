556968837183e4ce026bf24fe4d01ba28db591ee
dave-2112 <davemesfin16@gmail.com>
2022-09-23 23:43:25 +0300

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a has table
- * @size: size of the hash table array
+ * main - check the code for
  *
- * Description: creates a hash table having being passes the size
- *
- * Return: new created hash table
+ * Return: Always EXIT_SUCCESS.
  */
-hash_table_t *hash_table_create(unsigned long int size)
+int main(void)
 {
-	hash_table_t *new_hash_table;
-	unsigned long int i;
+    hash_table_t *ht;
 
-	new_hash_table = malloc(sizeof(hash_table_t));
-	if (new_hash_table == NULL)
-		return (NULL);
-
-	new_hash_table->size = size;
-	new_hash_table->array = malloc(sizeof(hash_table_t *) * size);
-	if (new_hash_table->array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		new_hash_table->array[i] = NULL;
-
-	return (new_hash_table);
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
 }
