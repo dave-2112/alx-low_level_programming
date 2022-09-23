89aec409a7f9642422cc12ac71509ac186c5d390
dave-2112 <davemesfin16@gmail.com>
2022-09-23 23:10:34 +0300

    Hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,36 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - gets the value of a key
+ * hash_table_get - Get a value from the hash table.
+ * @ht: Hash table.
+ * @key: Key.
  *
- * @ht: hash table
- * @key: key
- *
- * Description: returns the value of a key in a hash table
- *
- * Return: value, otherwise NULL
+ * Return: The value if works, NULL if doesn't.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *current;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
 		return (NULL);
 
-	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
-		return (NULL);
-
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
-	while (current)
+	if (current == NULL)
+		return (NULL);
+	while (strcmp(current->key, key) && current != NULL)
 	{
-		if (strcmp(current->key, key) == 0)
-		{
-			return (current->value);
-		}
 		current = current->next;
 	}
-
-	return (NULL);
+	if (current == NULL)
+		return (NULL);
+	else
+		return (current->value);
 }
